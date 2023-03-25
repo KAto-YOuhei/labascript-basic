4ed551cba38b5201bd3b5d805f8912b5164b2fc1
KAto-YOuhei <you42691@gmail.com>
2023-03-25 10:26:33 +0900

    syokai

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const sayGoodMorning = () => {
+  console.log("おはようございます！");
+  console.log("昨日はよく眠れましたか？");
+  console.log("今日も一日頑張りましょう！");
+};
+
+const sayGoodEvening = () => {
+  console.log("こんばんは！");
+  console.log("今日もお疲れさまでした。");
+};
+
+sayGoodMorning();
+
+sayGoodEvening();
+
+const calculateTotal = (price) => {
+  console.log(price + 500 + "円");
+};
+
+calculateTotal(1200);
+
+const addTwoArgument = (price, shippingFee) => {
+  console.log(price + shippingFee + "円");
+};
+
+addTwoArgument(1200, 500);
+
+const double = (num) => {
+  return num * 2;
+};
+
+console.log(double(30));
