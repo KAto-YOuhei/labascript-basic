8c6fe0ee3bea8b49b0044ab6ee2b3b7d78bb1707
KAto-YOuhei <you42691@gmail.com>
2023-03-27 22:27:27 +0900

    通常オブジェクトにメソッドを定義

--- js/class.js
+++ js/class.js
@@ -1,19 +1,3 @@
-//クラスを定義する
-class Product {
-  //インスタンス化すると同時に処理を実行（初期化）する
-  constructor() {
-    //インスタンス（オブジェクト）にプロパティを持たせる
-    this.name = "シャンプー";
-    this.price = 500;
-    this.category = "生活雑貨";
-  }
-}
-//インスタンス化する
-const shampoo = new Product();
-
-//インスタンス（オブジェクト）の値を出力するcons
-console.log(shampoo);
-
 //引数ありVer
 //クラスを定義する
 class Product {
@@ -22,6 +6,11 @@ class Product {
     this.name = name;
     this.price = price;
     this.category = category;
+  }
+
+  //メソッドを定義する
+  describe() {
+    console.log("この商品名は" + this.name + "です");
   }
 }
 
@@ -32,3 +21,18 @@ const coffee = new Product("コーヒー", 1500, "飲料");
 //インスタンス（オブジェクト）の値を出力する
 console.log(shampoo);
 console.log(coffee);
+
+shampoo.describe();
+
+//通常のオブジェクトにメソッドを定義する
+const user = {
+  name: "侍太郎",
+  age: 36,
+  gender: "男性",
+  greet: () => {
+    console.log("よろしくお願いします");
+  },
+};
+
+//メソッドを呼び出す（実行する）
+user.greet();
