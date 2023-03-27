bf33460b7523803dfb474e02e5b0b3c70a195fd7
KAto-YOuhei <you42691@gmail.com>
2023-03-27 22:13:10 +0900

    引数ありのクラス

--- js/class.js
+++ js/class.js
@@ -13,3 +13,22 @@ const shampoo = new Product();
 
 //インスタンス（オブジェクト）の値を出力するcons
 console.log(shampoo);
+
+//引数ありVer
+//クラスを定義する
+class Product {
+  constructor(name, price, category) {
+    //インスタンス（オブジェクト）にプロパティを持たせる
+    this.name = name;
+    this.price = price;
+    this.category = category;
+  }
+}
+
+//インスタンス化する
+const shampoo = new Product("シャンプー", 500, "生活雑貨");
+const coffee = new Product("コーヒー", 1500, "飲料");
+
+//インスタンス（オブジェクト）の値を出力する
+console.log(shampoo);
+console.log(coffee);
