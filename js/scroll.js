2574ae6fb3ae9e2d4f7c49bf57622e9a78ba9704
KAto-YOuhei <you42691@gmail.com>
2023-04-04 17:01:50 +0900

    miss 修正

--- js/scroll.js
+++ js/scroll.js
@@ -4,7 +4,7 @@ const backBtn = document.getElementById("back-btn");
 // 画面がスクロールされたときにイベント処理を実行する
 window.addEventListener("scroll", () => {
   //画面がスクロール量をpx(ピクセル)数で取得する
-  const scroll = document.scrollingElement.scrollTop;
+  const scrollValue = document.scrollingElement.scrollTop;
 
   //画面のスクロール量が300px以上であれば、「TOPに戻る」ボタンを表示する
   if (scrollValue >= 300) {
